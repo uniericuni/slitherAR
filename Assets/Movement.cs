8e3b587d6423a3e4ef3f262acba34adbd26eedb4
davidhu34 <b01901133@gmail.com>
2016-06-15 17:18:02 +0800

    dumb balls

---
+++ Assets/Movement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : Photon.MonoBehaviour
+{
+    private Rigidbody rb;
+    private Quaternion FixedAngle;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        FixedAngle = transform.rotation;
+    }
+
+    void FixedUpdate()
+    {
+        if (photonView.isMine)
+        {
+            Move();
+        }
+    }
+    private void Move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.transform.Translate(movement);
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+            stream.SendNext(rb.position);
+        else
+            rb.position = (Vector3)stream.ReceiveNext();
+    }
+}
---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NetworkManager : MonoBehaviour {
+    public InputField nameInput;
+    public Button enterRoom;
+    public Text rankName1;
+    public Text rankName2;
+    public Text rankName3;
+    public Text rankName4;
+    public Text rankName5;
+
+    private Text[] rankTexts;
+    private string errorMessage;
+    private string playerName;
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("1.7");
+        rankTexts = new Text[5] { rankName1, rankName2, rankName3, rankName4, rankName5 };
+    }
+    void Update()
+    {
+    }
+
+    void OnJoinedLobby()
+    {
+        RoomOptions roomOptions = new RoomOptions()
+        {
+            isVisible = false,
+            maxPlayers = 20
+        };
+        PhotonNetwork.JoinOrCreateRoom( "master", roomOptions, TypedLobby.Default);
+    }
+
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate( "balla", transform.position, transform.rotation, 0); 
+    }
+}
