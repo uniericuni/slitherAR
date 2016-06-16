378b689ee9116b90aaa7c429a899ef8aec37de9b
davidhu34 <b01901133@gmail.com>
2016-06-16 17:41:32 +0800

    MGR interface2

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,16 +4,21 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
+	private List<string> playerNames;
 	private Hashtable playerBodies;
-	private Hashtable playerHead;
 	private Hashtable playerScore;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
+
+
+	void FixedUpdate()
+	{
+		MoveBodies();
+	}
+
 	void Update () {
 	
 	}
@@ -22,11 +27,38 @@ public class GameManager : MonoBehaviour {
 	{
 		foreach (string n in names)
 		{
-			if
+			if (!playerNames.Contains(n))
+			{
+				playerNames.Add(n);
+				playerBodies.Add(n, new List<Transform>());
+				playerScore.Add(n, 0f);
+			}
 		}
 	}
 	public void updateScore(string name, float score)
 	{
 		playerScore[name] = score; 
+	}
+	public void addBody(string n, Transform t)
+	{
+		/*
+		 ... = instantiate a body on t
+		playerBodies[n].Add(...)
+		 * /
+	}
+
+	private void MoveBodies()
+	{
+		//wei fang god
+		foreach ( string bodies in playerBodies.Values)
+		{
+			/*
+			for (i = 0 ... bodies.size)
+			{
+				bodies[i].transform = bodies[i-1]......大概是這樣吧
+			}
+			*/
+		}
+	
 	}
 }
